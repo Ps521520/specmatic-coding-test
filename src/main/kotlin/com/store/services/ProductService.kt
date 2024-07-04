24749130cf6912ece9cfc038d0380210e7be8aab
Prashant <prashant.kumar@team-lease.co.in>
2024-07-05 00:44:50 +0530

    Add product service

---
+++ src/main/kotlin/com/store/services/ProductService.kt
@@ -0,0 +1,27 @@
+package com.store.services
+
+import com.store.models.Product
+import com.store.models.ProductType
+import java.util.concurrent.atomic.AtomicInteger
+import org.springframework.stereotype.Service
+
+@Service
+class ProductService {
+    private val products = mutableMapOf<Int, Product>()
+    private val idGenerator = AtomicInteger(1)
+
+    fun getProductsByType(type: ProductType): List<Product> {
+        return products.values.filter { it.type == type.toString() }
+    }
+
+    fun getAllProducts(): List<Product> {
+        return products.values.toList();
+    }
+
+    fun createProduct(details: Product): Int {
+        val id = idGenerator.getAndIncrement()
+        val product = Product(id, details.name, details.type, details.inventory)
+        products[id] = product
+        return id
+    }
+}
